b14594bb165925fbec58555f7fd602c2ff13883e
Ilya Danilovich <ilyadan98@gmail.com>
2021-02-20 15:59:42 +0300

    structure

---
+++ src/main/java/com/overone/final_project_Danilovich/blockc/BlockcApplication.java
@@ -0,0 +1,13 @@
+package com.overone.final_project_Danilovich.blockc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/overone/final_project_Danilovich/blockc/BlockcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overone.final_project_Danilovich.blockc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
